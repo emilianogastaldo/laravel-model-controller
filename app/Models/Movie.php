4added893230370cfe045f8bef14f42923d1f577
emilianogastaldo <emiliano0303@gmail.com>
2024-03-11 16:49:53 +0100

    aggiungo la funzione getDate al modello Movie

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+    public function getDate()
+    {
+        $date = date_format(date_create($this->date), "d M Y");
+        return $date;
+    }
 }
