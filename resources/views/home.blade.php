c2a0592ec3e363b0e0ccfc6dec9aeda8a2e34449
emilianogastaldo <emiliano0303@gmail.com>
2024-03-11 16:50:22 +0100

    ricreo la home con il layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+@extends('layouts.main')
 
-    @vite('resources/js/app.js')
-</head>
-<body>
-    {{dd($movies)}}
-    <h1>prova</h1>
-</body>
-</html>
+@section('title', 'Home')
+
+@section('header', 'Movies')
+@section('main-content')
+<div class="container">
+    <div class="row g-4">
+        @forelse ( $movies as $movie )
+        <div class="col-3">
+            @include('includes.card')
+        </div>                    
+        @empty
+            {{abort(404)}}
+        @endforelse
+    </div>
+</div>    
+@endsection
