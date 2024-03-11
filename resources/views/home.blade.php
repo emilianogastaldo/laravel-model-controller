56073ab2e69d577a13c558fd158734d0f0376176
emilianogastaldo <emiliano0303@gmail.com>
2024-03-11 15:40:32 +0100

    installed bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+
+    @vite('resources/js/app.js')
 </head>
 <body>
-    Prova
+    <h1>prova</h1>
 </body>
 </html>
